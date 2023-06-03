7d5a87cb81c2071e6d8dcc532af1cc72076247f3
97illi <0297kh@gmail.com>
2023-06-03 22:42:52 +0900

    po

--- class-site_starter-kit/p2/assets/function.js
+++ class-site_starter-kit/p2/assets/function.js
@@ -1,17 +1,20 @@
 function myFunction() {
-  // List of names
+  // names
   var names1 = ["Bride", "Heaven", "Temptation", "First", "Everybody", "Sky", "Penthouse", "Life", "Secret", "Mother"];
   var names2 = ["of the", "within", "and", "without", "Next to", "Wives", "Say", "Stairway", "in-law"];
   var names3 = ["Golden", "Club", "Wife", "Gisaeng", "Castle", "Kimchi", "Inheritance"];
+  //fonts
+  var fonts = ["serif", "sans-serf"];
   
-//random generate
   var randomNum1 = Math.floor(Math.random() * names1.length);
   var randomNum2 = Math.floor(Math.random() * names2.length);
   var randomNum3 = Math.floor(Math.random() * names3.length);
+  // fonts
+  var fonts = Math.floor(Math.random() * fonts.length);
   
-  // Get the name from the array using the random number
   var randomName = names1[randomNum1]+ " " + names2[randomNum2]+ " " + names3[randomNum3];
-  
-  // Display the name on the page
-  document.getElementById("name").innerHTML = randomName;
+  //fonts
+  var randomFont = fonts[Math.floor(Math.random() * fonts.length)];
+
+  document.getElementById("name").innerHTML = randomName + randomFont;
 }
