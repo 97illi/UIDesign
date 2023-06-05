17669d45e61e31bb2dc7129f90cd5950a5e4e9fb
97illi <0297kh@gmail.com>
2023-06-05 15:40:32 +0900

    hey

--- class-site_starter-kit/p2/assets/function.js
+++ class-site_starter-kit/p2/assets/function.js
@@ -1,20 +1,24 @@
 function myFunction() {
   // names
-  var names1 = ["Bride", "Heaven", "Temptation", "First", "Everybody", "Sky", "Penthouse", "Life", "Secret", "Mother"];
-  var names2 = ["of the", "within", "and", "without", "Next to", "Wives", "Say", "Stairway", "in-law"];
+  var names1 = ["<span class='big'>Bride</span>", "Heaven", "Temptation", "First", "Everybody", "Sky", "Penthouse", "Life", "Secret", "Mother"];
+  var names2 = ["<span class='small'>of the</span>", "<span class='small'>within</span>", "<span class='small'>and</span>", "<span class='small'>without</span>", "Next to", "Wives", "Say", "Stairway", "in-law"];
   var names3 = ["Golden", "Club", "Wife", "Gisaeng", "Castle", "Kimchi", "Inheritance"];
   //fonts
-  var fonts = ["serif", "sans-serf"];
+  var fonts = ["Alice", "Aoboshi", "Playfair", "Lobster", "Great Vibes", "Castoro", "Novacut", "Purple"];
   
   var randomNum1 = Math.floor(Math.random() * names1.length);
   var randomNum2 = Math.floor(Math.random() * names2.length);
   var randomNum3 = Math.floor(Math.random() * names3.length);
+ 
   // fonts
-  var fonts = Math.floor(Math.random() * fonts.length);
+  var randomFontNum1 = Math.floor(Math.random() * fonts.length);
+  var randomFontNum2 = Math.floor(Math.random() * fonts.length);
+  var randomFontNum3 = Math.floor(Math.random() * fonts.length);
+  var randomFont1 = fonts[randomFontNum1];
+  var randomFont2 = fonts[randomFontNum2];
+  var randomFont3 = fonts[randomFontNum3];
   
-  var randomName = names1[randomNum1]+ " " + names2[randomNum2]+ " " + names3[randomNum3];
-  //fonts
-  var randomFont = fonts[Math.floor(Math.random() * fonts.length)];
 
-  document.getElementById("name").innerHTML = randomName + randomFont;
+  var randomName = "<span class='"+randomFont1+"'> <span style='font-family: "+randomFont1+"'>"+names1[randomNum1]+ "</span> <span style='font-family: "+randomFont2+"'>"+names2[randomNum2]+ "</span> <span style='font-family: "+randomFont3+"'>"+ names3[randomNum3]+"</span>";
+  document.getElementById("name").innerHTML = randomName;
 }
